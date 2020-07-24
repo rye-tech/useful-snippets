2ee5fb2b18968c544dd9a4eed541888af0805581
Ryan J. Hartnett <rynocean@gmail.com>
2020-07-24 15:24:44 -0700

    just getting started

---
+++ data-wrangling.R
@@ -0,0 +1,26 @@
+# useful code for data wrangling #######
+
+
+# check data types of variables with class()
+# example:
+
+x <- 4 # take note, the old value assigned to x is replaced by 4
+x
+class(x)
+
+text1 <- "Yarrr"
+class(text1)
+
+
+logic1 <- TRUE 
+class(logic1)
+
+
+
+
+
+
+
+
+
+
